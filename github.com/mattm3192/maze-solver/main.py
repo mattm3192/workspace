520a98ea7663e18c05e49c9612c2ceb81c3d927e
mattm3192 <mattm3192@gail.com>
2025-04-15 15:00:13 -0500

    Lesson 4 complete

--- github.com/mattm3192/maze-solver/main.py
+++ github.com/mattm3192/maze-solver/main.py
@@ -1,7 +1,18 @@
-from graphics import Window
+from graphics import *
 
 def main():
 	win = Window(800, 600)
+	p1 = Point(10, 30)
+	p2 = Point(140, 150)
+	p3 = Point(400, 300)
+	p4 = Point(240,150)
+	l1 = Line(p1, p2)
+	l1.draw(win.canvas)
+	l2 = Line(p3, p4)
+	l2.draw(win.canvas, "red")
+	l3 = Line(p1, p3)
+	l3.draw(win.canvas, "blue")
+
 	win.wait_for_close()
 
 main()
